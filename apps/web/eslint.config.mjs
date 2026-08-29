8165ff6ce82a0d11f7b3690e7520f287341d209a
Kandil122 <AdhamKandil21@gmail.com>
2026-08-29 22:51:55 +0300

    Integrate changes

--- apps/web/eslint.config.mjs
+++
@@ -1,3 +0,0 @@
-import config from "@sanad/config/eslint";
-
-export default config;
--- apps/web/tailwind.config.mjs
+++
@@ -1,7 +0,0 @@
-import preset from "@sanad/config/tailwind";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  presets: [preset],
-  content: ["./src/**/*.{ts,tsx}"],
-};
